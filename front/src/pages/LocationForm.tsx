10c8fa619e4070fcd2519ae4bf4cb79b5d920a56
Killian Mannarelli <kmannarelli@ensc.fr>
2024-10-18 16:18:20 +0200

    feat: begin location

--- front/src/pages/LocationForm.tsx
+++ front/src/pages/LocationForm.tsx
@@ -1,8 +1,22 @@
+import { Grid2, Typography } from "@mui/material"
+import { DatePicker } from "@mui/x-date-pickers"
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+
 
 
 const LocationForm = () => {
   return (
-    <div>LocationForm</div>
+    <Grid2 container spacing={2} direction={"column"}>
+        <Grid2>
+      <Typography variant="h4">Location</Typography>
+      </Grid2>
+      <Grid2>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DatePicker label="Basic date picker" />
+      </LocalizationProvider>
+      </Grid2>
+    </Grid2>
   )
 }
 
