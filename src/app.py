efed76817b1d5308711ffe26acddf4e259f3048d
Lucie Perrin <lucie.perrin@enseirb-matmeca.fr>
2024-11-15 16:32:16 +0100

    modif app

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 from weather import WeatherAPIcurrent
 from dotenv import load_dotenv
 import os
+#from flask import Flask, render_template, request
 
 load_dotenv()
 api_key = os.getenv("API_KEY")
@@ -12,3 +13,13 @@ meteo_bdx = weather.get_weather('44.84018003833398', '-0.5774165946831533')
 print(meteo_bdx)
 print(meteo_bdx['visibility'])
 
+
+#app = Flask(__name__)
+
+# @app.route('/data', methods=['POST'])
+# def get_data():
+#     data = request.get_json()
+#     latitude = data['latitude']
+#     longitude = data['longitude']
+#     date = data['date']
+#     print(latitude)
