0fd63472740a8339e1c9e67e780c60a975d28fa1
Pyrhanox <allandenoce@gmail.com>
2024-11-29 14:39:06 +0100

    IA

--- src/ml.py
+++ src/ml.py
@@ -1,21 +1,39 @@
-#import tensorflow as tf
-#from tensorflow.keras.models import Sequential
-#from tensorflow.keras.layers import Dense
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
 from sklearn.model_selection import train_test_split
-#from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
 import numpy as np
-import csv 
 from data_for_ml import create_matrix
+from sklearn.preprocessing import LabelEncoder
 
 X, y = create_matrix()
+
+encoder = LabelEncoder()
+y = encoder.fit_transform(y.flatten())
+
+y = tf.keras.utils.to_categorical(y, num_classes=np.max(y) + 1)
 
 scaler = StandardScaler()
 X = scaler.fit_transform(X)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, shuffle=False)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, shuffle=True)
 
+model = Sequential()
+model.add(Dense(256, input_dim=X.shape[1], activation='relu'))
+model.add(Dense(128, activation='relu')) 
+model.add(Dense(64, activation='relu')) 
+model.add(Dense(32, activation='relu')) 
+model.add(Dense(16, activation='relu')) 
+model.add(Dense(y.shape[1], activation='softmax'))
 
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
+history = model.fit(X_train, y_train, epochs=30, batch_size=16, validation_split=0.5, verbose=1)
+
+loss, accuracy = model.evaluate(X_test, y_test, verbose=0)
+print(f"Test Loss: {loss:.4f}")
+print(f"Test Accuracy: {accuracy:.4f}")
 
 
 
