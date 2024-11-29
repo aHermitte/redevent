66aa695de04802a4de344b166b4028528a9bfba7
Jules Braun <jules.braun38@gmail.com>
2024-11-29 14:59:26 +0100

    .gitignore with csv Keolis + weather for a date

--- src/weather.py
+++ src/weather.py
@@ -34,3 +34,66 @@ class WeatherAPIcurrent:
             print(f"Error fetching weather data: {e}")
             return None
 
+
+    def get_weather_at_date(self, latitude, longitude, date):
+        """
+        Get weather data based on latitude and longitude at a specific date (5 days after current date maximum).
+
+        Args:
+        - latitude (float): Latitude coordinate of the location.
+        - longitude (float): Longitude coordinate of the location.
+        - date (string)
+
+        Returns:
+        - city information + average weather (array): Array containing weather prediction at the date in parameter.
+        """
+
+        weather_data = get_weather(self, latitude, longitude)
+
+        # Get "List" part of the Json
+        forecast_list = weather_data.get("list", [])
+
+        # Convert date to datetime
+        target_date = datetime.strptime(date, "%Y-%m-%d %H:%M:%S")
+
+        # Initialize block before and after the date in parameter
+        before_block = None
+        after_block = None
+
+        # Find those blocks
+        for forecast in forecast_list:
+            forecast_date = datetime.strptime(forecast["dt_txt"], "%Y-%m-%d %H:%M:%S")
+
+            if forecast_date <= target_date:
+                before_block = forecast
+            elif forecast_date > target_date and after_block is None:
+                after_block = forecast
+                break
+
+        if not before_block or not after_block:
+            raise ValueError("Impossible to find blocks before and after the dte in parameter.")
+
+        # Initialize average block
+        averaged_block = {}
+
+        for key in before_block.keys():
+            # Average of numeral values
+            if isinstance(before_block[key], (int, float)) and isinstance(after_block[key], (int, float)):
+                averaged_block[key] = (before_block[key] + after_block[key]) / 2
+            elif isinstance(before_block[key], dict) and isinstance(after_block[key], dict):
+                averaged_block[key] = {
+                    sub_key: (before_block[key][sub_key] + after_block[key][sub_key]) / 2
+                    for sub_key in before_block[key]
+                    if isinstance(before_block[key][sub_key], (int, float)) and isinstance(after_block[key][sub_key], (int, float))
+                }
+            else:
+                # Text values : we keep value of before_block
+                averaged_block[key] = before_block[key]
+
+        # Get city values
+        city_data = weather_data.get("city", {})
+
+        # Return city values and the average block
+        return city_data | averaged_block
+
+
