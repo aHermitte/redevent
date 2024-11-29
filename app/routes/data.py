a64dda02736695237fae2c41a810bdccf5dcb50d
aHermitte <ahermitte@enseirb-matmeca.fr>
2024-11-29 14:30:40 +0100

    fausse réponse de la route pour l'affichage du front

--- app/routes/data.py
+++ app/routes/data.py
@@ -1,11 +1,18 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Blueprint, jsonify
 import requests
 
-app = Flask(__name__)
+data_bp = Blueprint('data', __name__)
 
-@app.route('/data', methods=['POST'])
+@data_bp.route('/data', methods=['POST'])
 def get_data():
-    data = request.get_json()
-    latitude = data['latitude']
-    longitude = data['longitude']
-    date = data['date']
+    # Example logic to handle the POST request
+    content = request.get_json()
+    if not content:
+        return jsonify({"error": "No JSON data received"}), 400
+    return return_model_predictions(content)
+
+def return_model_predictions(input):
+    print(input)
+    # Replace this with actual model
+    result = jsonify({"incidents": [{"latitude": 44, "longitude": 44, "description": "description de l'incident"}]})
+    return result
