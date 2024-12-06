6bdcf8a75e516ae6b1ca71b66fc53efdc57888fb
Lucie Perrin <lucie.perrin@enseirb-matmeca.fr>
2024-12-06 10:01:22 +0100

    fonction weather at date plus simple

--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
+from datetime import date, datetime
 from weather import WeatherAPIcurrent
 from dotenv import load_dotenv
 import os
@@ -7,8 +8,13 @@ api_key = os.getenv("API_KEY")
 
 weather = WeatherAPIcurrent(api_key)
 meteo_bdx = weather.get_weather('44.84018003833398', '-0.5774165946831533')
+meteo_date = weather.get_forecast_at_datetime('44.84018003833398', '-0.5774165946831533', datetime(2024, 12, 7, 8, 30))
 
 
 print(meteo_bdx)
 print(meteo_bdx['visibility'])
+print(meteo_date)
+print(meteo_date['main']['temp'])
+print(meteo_date['rain']['3h'])
+print(meteo_date['wind']['speed'])
 
