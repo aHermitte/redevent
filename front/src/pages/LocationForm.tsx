eadad8997df3b1bb1ef81506f67fc3e80f570d0d
aHermitte <ahermitte@enseirb-matmeca.fr>
2025-01-14 16:46:07 +0100

    feat: display result directly on the page

--- front/src/pages/LocationForm.tsx
+++ front/src/pages/LocationForm.tsx
@@ -35,12 +35,14 @@ const LocationForm = () => {
           "Content-Type": "application/json",
         },
         body: JSON.stringify(data),
+        // TODO: Use and display received data
       });
 
       if (response.ok) {
         alert("Data sent successfully");
         response.json().then((data) => {
           console.log(data);
+          handleResult(data);
           setIncidents(data.incidents);
         });
       } else {
@@ -52,37 +54,56 @@ const LocationForm = () => {
     }
   };
 
+  const handleResult = async (result: any) => {
+    document.getElementById("time_result")!.innerText = result.input[0].time;
+    document.getElementById("date_result")!.innerText = result.input[0].date;
+    document.getElementById("position_result")!.innerText = `Latitude: ${result.input[0].latitude}, Longitude: ${result.input[0].longitude}`;
+    document.getElementById("probability_result")!.innerText = result.proba[0].prob_accident;
+  };
+
   return (
-    <Grid2 container spacing={2} direction={"column"}>
-      <Grid2>
-        <Typography variant="h4">Location</Typography>
+    <Grid2 container direction={"row"}>
+      <Grid2 container spacing={2} direction={"column"}>
+        <Grid2>
+          <Typography variant="h4">Location</Typography>
+        </Grid2>
+        <Grid2>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker
+              label="Basic date picker"
+              value={date}
+              onChange={(newDate) => setDate(newDate)}
+            />
+            <TimePicker
+              label="Basic time picker"
+              ampm={false}
+              value={time}
+              onChange={(newTime) => setTime(newTime)}
+            />
+          </LocalizationProvider>
+        </Grid2>
+        <Grid2>
+          <Typography variant="h5">Map</Typography>
+          <Map onPositionChange={setPosition} markedPositions={incidents} />
+          <Button
+            variant="contained"
+            onClick={() => {
+              sendDatatoBack(position, date, time);
+            }}
+          >
+            Send data to back
+          </Button>
+        </Grid2>
       </Grid2>
       <Grid2>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DatePicker
-            label="Basic date picker"
-            value={date}
-            onChange={(newDate) => setDate(newDate)}
-          />
-          <TimePicker
-            label="Basic time picker"
-            ampm={false}
-            value={time}
-            onChange={(newTime) => setTime(newTime)}
-          />
-        </LocalizationProvider>
-      </Grid2>
-      <Grid2>
-        <Typography variant="h5">Map</Typography>
-        <Map onPositionChange={setPosition} markedPositions={incidents} />
-        <Button
-          variant="contained"
-          onClick={() => {
-            sendDatatoBack(position, date, time);
-          }}
-        >
-          Send data to back
-        </Button>
+        <Typography variant="h4">Results</Typography>
+        <Typography variant="h5">Parameters</Typography>
+        <p id="date_result"></p>
+        <p id="time_result"></p>
+        <p id="position_result"></p>
+        <Typography variant="h5">Probability</Typography>
+        <p id="probability_result"></p>
+
       </Grid2>
     </Grid2>
   );
